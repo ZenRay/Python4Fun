0e0f173c8b404c285a2d72944c668094343138fe
ZenRay <jake2011ren@gmail.com>
2019-04-19 03:34:03 +0800

    feat($ImgToChar $Transform): Convert the pixel value to string

--- ImgToChar/imgToChar.py
+++ ImgToChar/imgToChar.py
@@ -5,6 +5,8 @@
 import argparse
 from PIL import Image
 import string
+import os
+import re
 
 # TODO: Get the argument with user interactive
 
@@ -17,8 +19,8 @@ parser.add_argument(
     "-f", "--file", action="append", 
     help="img file or list of img files. eg:-f file1 -f file2"
 )
-parser.add_argument("--width", type=int, default=100)
-parser.add_argument("--height", type=int, default=100)
+parser.add_argument("--width", type=int, default=80)
+parser.add_argument("--height", type=int, default=80)
 
 ## ⏰if required True, must specify the argument
 parser.add_argument("-o", "--output", required=False)
@@ -32,11 +34,10 @@ OUTPUT = args.output
 WIDTH = args.width
 HEIGHT = args.height
 
-print(args)
 # TODO: Transform RGB as grey
-def __scale_pixel(image, width=WIDTH, height=HEIGHT, resize=False, crop=False):
+def __scale_pixel(file, width=WIDTH, height=HEIGHT, resize=False, crop=False):
     # TODO: open image
-    img = Image.open(image)
+    img = Image.open(file)
 
     # TODO: check whether the arguments width and height are right
     # Cutting or resizing image
@@ -54,9 +55,42 @@ def __scale_pixel(image, width=WIDTH, height=HEIGHT, resize=False, crop=False):
     if crop:
         img = img.crop((width, height))
     if resize:
-        img = img.resize((width, height))
+        img = img.resize((width, height), Image.NEAREST)
 
     # get the grey image
     img = img.convert("L")
 
     return (width, height), img
+
+# TODO: Transform the img to text
+def __transform(file, verbose=True, output=None, save=False, **argv):
+    _, img = __scale_pixel(file, **argv)
+    # img.show()
+    result = ""
+    for y in range(img.size[1]):
+        for x in range(img.size[1]):
+            index = int(img.getpixel((x,y)) / 257.0 * len(CHAR))
+            result += CHAR[index]
+        result += "\n"
+    
+    if verbose:
+        print(result)
+    
+    if save:
+        try:
+            if output is None:
+                output = re.sub(r"\.[a-z]+", ".txt", file)
+            elif not output.endswith(".txt"):
+                output = os.path.join(output, re.sub(r"\.[a-z]+", ".txt", file))
+        except AttributeError as err:
+            print(err + "Need store filename")
+            output = input("Plz enter text file name: ")
+            while not output.endswith(".txt"):
+                print("Enter a file tail name is .txt!")
+                output = input("Plz enter text file name: ")
+        
+        with open(output, "w") as file:
+            file.write(result)
+
+if __name__ == "__main__":
+    __transform(IMAGE[0], resize=True)
