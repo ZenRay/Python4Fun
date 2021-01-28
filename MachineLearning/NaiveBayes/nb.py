0b3de1e23e3e042f6a0808fd3e53e52d65000db6
ZenRay <jake2011ren@gmail.com>
2021-01-29 01:22:55 +0800

    feat($naivebayes): Naive Bayes Predict
    朴素贝叶斯预测，目前暂时未解决连续数据预测

--- MachineLearning/NaiveBayes/nb.py
+++ MachineLearning/NaiveBayes/nb.py
@@ -4,14 +4,15 @@
 """
 from collections import defaultdict, namedtuple
 from sklearn.base import TransformerMixin, BaseEstimator
-
+import numpy as np
+from functools import reduce
 # 定义条件概率的键类型，分别记录了:
 # action: 反应类型，这里指条件概率的 P(A|B) 中的 A
 # condition: 条件类型，这里指条件概率 P(A|B) 中的 B
 # index: 维度索引
-field = namedtuple("cp", "action condition index")
+field = namedtuple("cp", "action condition index name", defaults=[None])
 
-class NaiveBayes(TransformerMixin, BaseEstimator):
+class NaiveBayes(BaseEstimator):
     def __init__(self) -> None:
         super().__init__()
 
@@ -25,8 +26,8 @@ class NaiveBayes(TransformerMixin, BaseEstimator):
 
         for label in y:
             self.prior_[label] += 1 / len(y)
-        
-        for axis in X.shape[1]:
+        # 计算条件概率
+        for axis in range(X.shape[1]):
             for action, condition in zip(X[:, axis], y):
                 self.condition_[field(action, condition, axis)] += (
                                             1 / len(y) / self.prior_[condition])
@@ -34,10 +35,32 @@ class NaiveBayes(TransformerMixin, BaseEstimator):
         return self
 
     
+    def predict(self, X):
+        result = np.full((len(X), 1), np.nan)
+        labels = list(self.prior_.keys())
+        for row, point in enumerate(X):
+            index = np.argmax(self.single_point(point))
+            result[row, 0] = labels[index]
+        return result
+
+    
+    def single_point(self, point):
+        """预测单个数据点分类"""
+        result = []
+        for label in self.prior_:
+            # 拆分计算 $\prod_{j=1}^n=P(x^{(j)}|y=\text{label})$ 
+            value = reduce(lambda x, y: x* y, [self.condition_[field(action, label, index)]
+                 for index, action in enumerate(point)]
+            )
+            # 计算该分类下的得分——不同类别的各特征条件概率的乘积再与先验概率相乘得到各个
+            result.append(self.prior_[label] * value)
+        return result
+
     
 
 if __name__ == "__main__":
     import io
+    import pandas as pd
     data = io.StringIO("""x1,x2,y
     1,S,-1
     1,M,-1
@@ -55,5 +78,11 @@ if __name__ == "__main__":
     3,L,1
     3,L,-1
     """)
+    df = pd.read_csv(data)
+    X = df.iloc[:2, :].to_numpy()
+    y = df.loc[:, "y"].to_numpy()
     
-    
+    model = NaiveBayes()
+    model.fit(X, y)
+    result = model.predict(np.array([[2, "S"]]))
+    print(result)
